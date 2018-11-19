2ae18e9764f4e0b0da26ec3679085f5f8bbc2e5f
songqibin <songqibin@jd.com>
2018-11-19 10:52:01 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Listen to fetch events
 self.addEventListener('fetch', function (event) {
   // Check if the image is a jpeg
-  if (/(\.jpe?g|png)\?iswebp$/.test(event.request.url)) {
+  if (/(\.jpe?g|\.png)\?iswebp$/.test(event.request.url)) {
     // Inspect the accept header for WebP support
     var supportsWebp = false;
     if (event.request.headers.has('accept')) {
@@ -13,8 +13,8 @@ self.addEventListener('fetch', function (event) {
       // Clone the request
       var req = event.request.clone();
       // Build the return URL
-      // var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
-      var returnUrl = req.url + '.webp'
+      var returnUrl = req.url.substr(0, req.url.lastIndexOf("?")) + ".webp";
+      // var returnUrl = req.url + '.webp'
       event.respondWith(
         fetch(returnUrl, {
           mode: 'no-cors'
